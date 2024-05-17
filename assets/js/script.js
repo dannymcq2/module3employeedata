af446a06882311f09dac627feaac3f315b145952
dannymcq2 <166949494+dannymcq2@users.noreply.github.com>
2024-05-16 20:27:27 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-const formContainer = document.querySelector('.form-container');
 const EmployeeTable = document.querySelector('#employee-table');
 let employees = [];
 
@@ -63,11 +62,6 @@ const getRandomEmployee = function(employeesArray) {
   console.log(`Congratulations to ${randomEmployee.firstName}, our random drawing winner!`);
 };
 
-addEmployeesBtn.addEventListener('click', function() {
-  const employees = collectEmployees();
-  trackEmployeeData(employees);
-});
-
 /*
   ====================
   STARTER CODE
@@ -109,6 +103,7 @@ const displayEmployees = function(employeesArray) {
   }
 };
 
+// Define trackEmployeeData here
 const trackEmployeeData = function(employees) {
   console.table(employees);
 
